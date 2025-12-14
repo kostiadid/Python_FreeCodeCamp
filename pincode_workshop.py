eefc62c278c802ec22a81d9d8b8561b9bd09cd46
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2025-12-14 20:14:23 +0000

    len(words) > line_index

--- pincode_workshop.py
+++ pincode_workshop.py
@@ -3,13 +3,13 @@ def pin_extractor(poem):
     lines = poem.split('\n')
     for line_index, line in enumerate(lines):
         words = line.split()
-        secret_code += str(len(words[line_index]))
-    
+        if len(words) > line_index:
+            secret_code += str(len(words[line_index]))
     return secret_code
-
 
 poem = """Stars and the moon
 shine in the sky
-white and bright
+white and
 until the end of the night"""
+
 print(pin_extractor(poem))
