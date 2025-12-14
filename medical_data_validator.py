878fa0e5b5e0339535f4e584dd4324c29708d055
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2025-12-14 21:10:45 +0000

    more  pairs

--- medical_data_validator.py
+++ medical_data_validator.py
@@ -1,5 +1,11 @@
 medical_records = [
     {
-        'patient_id': 'P1001'
+        'patient_id': 'P1001',
+        'age': 34,
+        'gender': 'Female',
+        'diagnosis':'Hypertension',
+        'medications' : ['Lisinopril'],
+        'last_visit_id' : 'V2301'
+
     }    
 ]
