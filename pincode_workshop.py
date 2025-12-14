66a75531c84887cac6c054e057f8946075ace712
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2025-12-14 20:03:20 +0000

    now we use our secret_code  variable

--- pincode_workshop.py
+++ pincode_workshop.py
@@ -4,7 +4,7 @@ def pin_extractor(poem):
     for line_index, line in enumerate(lines):
         print(line_index, line)
         words = line.split()
-        print(str(len(words[line_index])))
+        secret_code+=(str(len(words[line_index])))
 
 
 poem = """Stars and the moon
