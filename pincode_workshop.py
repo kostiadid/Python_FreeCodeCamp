be3134826f536c1c83a29dbb0d7043ce31c024a5
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2025-12-14 20:06:14 +0000

    secred code is index of lines

--- pincode_workshop.py
+++ pincode_workshop.py
@@ -2,14 +2,14 @@ def pin_extractor(poem):
     secret_code = ''
     lines = poem.split('\n')
     for line_index, line in enumerate(lines):
-        print(line_index, line)
         words = line.split()
-        secret_code+=(str(len(words[line_index])))
+        secret_code += str(len(words[line_index]))
+    
+    return secret_code
 
 
 poem = """Stars and the moon
 shine in the sky
 white and bright
 until the end of the night"""
-
-pin_extractor(poem)
+print(pin_extractor(poem))
