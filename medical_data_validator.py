f9158b942cb8082ad5b61cbef01a54f92b32809a
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2025-12-16 00:36:09 +0000

    add validation    parameters  from dictionary

--- medical_data_validator.py
+++ medical_data_validator.py
@@ -40,12 +40,14 @@ medical_records = [
 def find_invalid_records(
     patient_id, age, gender, diagnosis, medications, last_visit_id
 ):
-
     constraints = {
-        'patient_id': isinstance(patient_id, str) and re.fullmatch('p\d+', patient_id, re.IGNORECASE,),
-        'age':isinstance(age,int)
+        'patient_id': isinstance(patient_id, str) and re.fullmatch('p\d+', patient_id, re.IGNORECASE),
+        'age': isinstance(age, int) and age >= 18,
+        'gender': isinstance(gender, str) and gender.lower() in ('male', 'female'),
+        'diagnosis': isinstance(diagnosis, str) or diagnosis is None,
+        'medications': isinstance(medications, list) and all([isinstance(i, str) for i in medications]),
+        'last_visit_id':isinstance(last_visit_id,str)
     }
-
     return constraints
 
 def validate(data):
