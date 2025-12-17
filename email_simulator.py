d148626f715cb58419e81498c0415400ceceab08
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2025-12-16 21:33:15 -0500

    a few   methods    for self testing

--- email_simulator.py
+++ email_simulator.py
@@ -4,7 +4,14 @@ class Email:
         self.receiver = receiver
         self.subject = subject
         self.body = body
+        self.read = False
 
-email_obj = Email('alice@example.com','bob@example.com','Hello','Hi Bob!')
+    def mark_as_read(self):
+        self.read = True
+
+email_obj = Email('alice@example.com', 'bob@example.com', 'Hello', 'Hi Bob!')
 print(email_obj.sender)
 print(email_obj.subject)
+print(email_obj.read)
+email_obj.mark_as_read()
+print(email_obj.read)
