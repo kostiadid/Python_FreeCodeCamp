582f27cd9a844d929d38524bd526a2dccb28344d
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2025-12-16 21:23:05 -0500

    __init__ in python

---
+++ email_simulator.py
@@ -0,0 +1,7 @@
+class Email:
+    def __init__(self, sender, receiver, subject, body):
+        self.sender = sender
+        self.receiver = receiver
+        self.subject = subject
+        self.body = body
+        
