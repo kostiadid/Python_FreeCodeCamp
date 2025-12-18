67cfd0b70b5d6a502f7c34318e5b011557bad451
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2025-12-17 21:03:25 -0500

    CRUD  def

--- email_simulator.py
+++ email_simulator.py
@@ -1,3 +1,5 @@
+
+
 class Email:
     def __init__(self, sender, receiver, subject, body):
         self.sender = sender
@@ -42,8 +44,26 @@ class Inbox:
             print('Your inbox is empty.\n')
             return
         print('\nYour Emails:')
-        for i, email in  enumerate(self.emails,start=1):
+        for i, email in enumerate(self.emails, start=1):
             print(f'{i}. {email}')
-            
-            
-        
+
+    def read_email(self, index):
+        if not self.emails:
+            print('Inbox is empty.\n')
+            return
+        actual_index = index - 1
+        if actual_index < 0 or actual_index >= len(self.emails):
+            print('Invalid email number.\n')
+            return
+        self.emails[actual_index].display_full_email()
+
+    def delete_email(self, index):
+        if not self.emails:
+            print('Inbox is empty.\n')
+            return
+        actual_index = index - 1
+        if actual_index < 0 or actual_index >= len(self.emails):
+            print('Invalid email number.\n')
+            return
+        del self.emails[actual_index]
+        print('Email deleted.\n')
