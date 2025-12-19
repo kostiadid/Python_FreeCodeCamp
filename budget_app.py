7b0fa3b1d31a933c16b5b91a40ea7925054aa63e
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2025-12-18 21:39:32 -0500

    Implement Category class for budget management
    Added Category class with deposit, get_balance, and withdraw methods.

--- budget_app.py
+++ budget_app.py
@@ -1,1 +1,26 @@
+class Category:
 
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+
+    def deposit(self,amount , description=""):
+        self.ledger.append({'amount':amount, 'description':description})
+    
+    def get_balance(self):
+        total = 0
+        for item in  self.ledger:
+            total+= item['amount']
+        return(total)
+
+
+    def withdraw(self, amount, description=""):
+        balance = self.get_balance()
+        if  balance >= amount:
+            self.ledger.append({'amount':-amount, 'description':description})
+            return True  
+        return False
+    
+
+def create_spend_chart(categories):
+    pass
