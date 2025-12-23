870ef153ae16dbd24bbfdfa4707dba9b833c7d7a
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2025-12-23 13:41:03 -0500

    Update implement-the-bisection-method.py

--- implement-the-bisection-method.py
+++ implement-the-bisection-method.py
@@ -1,8 +1,6 @@
-def square_root_bisection(number,error,iterations=1000):
-
-    if number < 0:
+def square_root_bisection(num, tolerance=0.01, max_iter=100):
+    if num < 0:
         raise ValueError('Square root of negative number is not defined in real numbers')
-    if number == 0 or  number == 1 :
-        return f"The square root of {number} is {number}"
-    root = number ** 0.5
-    return f"The square root of {square_target} is approximately {root}"
+    elif num == 0 or num == 1:
+        print(f'The square root of {num} is {num}')
+        return num
