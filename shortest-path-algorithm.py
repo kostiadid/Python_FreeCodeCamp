6c1982c6872f08d29024c691f30393c08762adee
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2025-12-26 23:31:49 -0400

    Implement shortest path algorithm with adjacency matrix

---
+++ shortest-path-algorithm.py
@@ -0,0 +1,16 @@
+INF = float('inf')
+adj_matrix = [
+    [0, 5, 3, INF, 11, INF],
+    [5, 0, 1, INF, INF, 2],
+    [3, 1, 0, 1, 5, INF],
+    [INF, INF, 1, 0, 9, 3],
+    [11, INF, 5, 9, 0, INF],
+    [INF, 2, INF, 3, INF, 0],
+]
+
+def shortest_path(matrix, start_node, target_node=None):
+    n = len(matrix)
+    distances = [INF] * n
+    distances[start_node] = 0
+    paths = [[node_no] for node_no in range(n)]
+    visited = [False] * n
