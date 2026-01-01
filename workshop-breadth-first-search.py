46d2fd2181fe75d8f78ce6120bb0c0a5a79cd5d2
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2025-12-31 22:01:31 -0400

    Add queue initialization and debug print statement
    Initialize queue for breadth-first search and add debug print.

--- workshop-breadth-first-search.py
+++ workshop-breadth-first-search.py
@@ -4,5 +4,11 @@ def gen_parentheses(pairs):
     if pairs < 1:
         return 'The number of pairs should be at least 1'
     
+    queue = [('', 0, 0)]
     result = []
+
+    while queue:
+        print(queue)
+        current, opens_used, closes_used = queue.pop(0)
+
     return result
