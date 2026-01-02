34547d2c2be5ec8c6bd0cfd143e730f91386058f
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-01 23:07:01 -0400

    Update lab-n-queens-problem.py

--- lab-n-queens-problem.py
+++ lab-n-queens-problem.py
@@ -8,15 +8,34 @@ def dfs_n_queens(n):
         return [[0]]
     matrix = [[0 for _ in range(n)] for _ in range(n)]    
     pos = [0 for _ in range(n)]
-    matrix[0][0] = 1
-    prev_row = matrix[0]
-    prev_col = matrix[0][0]
 
-    # only one in col 
-    if cur_col == prev_col:
-        continue
-    # only one in diagonal
-    if abs(cur_col - prev_col) == abs(cur_row - prev_row):
-        continue
+    matrix[0][0] = 1
+
+    curr = 0
+    prev_row = 0
+    def move(curr):
+        if curr == n:
+            return
+        for j in range(n):
+                confl  = False
+                prev_col = matrix[prev_row].index(1)
+                if  j == prev_col:
+                    confl = True
+                    break
+                if abs(j - prev_col) == abs(i - prev_row):
+                    confl = True
+                    break
+                if confl:
+                    continue
+            
+                matrix[curr][j] = 1
+                move(curr+1)
+                matrix[curr][j] = 0
         
+         
+
+    move(curr+1)
+    print(matrix)
     return matrix
+
+dfs_n_queens(5)  
