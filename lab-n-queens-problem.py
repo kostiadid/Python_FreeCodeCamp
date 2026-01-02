4d0de00a8c809a6c7d495a55f389d36b582a7dcc
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-01 22:08:08 -0400

    Update lab-n-queens-problem.py

--- lab-n-queens-problem.py
+++ lab-n-queens-problem.py
@@ -8,10 +8,37 @@ def dfs_n_queens(n):
         return [[0]]
     matrix = [[0 for _ in range(n)] for _ in range(n)]
     
-    
+    pos = [0 for _ in range(n)]
 
+    # for q in range (n):
+    matrix[0][0] = 1
+    prev_row = matrix[0]
+    prev_col = matrix[0][0]
+
+    for q in range (n,2):
+        cur_row = [q]
+        cur_col = [q] 
+        if cur_col == prev_col:
+            continue
     return matrix
 
 
 
-# Queens of the same color don't hit each other, that's the solution, guys... (joke)
+# prev_row 
+# current_row
+#  prev_col
+#  prev_row
+#if abs(candidate_row - prev_row) == abs(candidate_col - prev_col):
+#     safe = True
+# for prev_row in range(current_row):
+#     prev_col = solution[prev_row]
+    
+#     # вертикаль
+#     if candidate_col == prev_col:
+#         safe = False
+#         break
+
+#     # диагонали
+#     if abs(candidate_col - prev_col) == abs(current_row - prev_row):
+#         safe = False
+#         break
