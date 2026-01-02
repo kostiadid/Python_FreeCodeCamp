288720a9fe8973f4f8661f11b2851a410fca86f2
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-02 16:07:44 -0400

    Update build-an-nth-fibonacci-number-calculator.py

--- build-an-nth-fibonacci-number-calculator.py
+++ build-an-nth-fibonacci-number-calculator.py
@@ -1,7 +1,16 @@
 def fibonacci(n):
-    if n < 0 or not isinstance(n,int):
-        return "Error"
     sequence = [0, 1]
+    if n < 0 or not isinstance(n, int):
+        return "Error"
+    prev1 = 0
+    prev2 = 1
+    if n == 0:
+        return prev1
+    if n == 1:
+        return prev2
+    for i in range(n - 1):
+        current = prev1 + prev2
+        prev1 = prev2
+        prev2 = current
+    return prev2
 
-    def sequence_func (n):
-        
