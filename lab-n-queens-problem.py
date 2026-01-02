13a1adfcfa5eba037bd5578d726e0ad5532a6abb
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-01 21:07:20 -0400

    Update lab-n-queens-problem.py

--- lab-n-queens-problem.py
+++ lab-n-queens-problem.py
@@ -1,1 +1,17 @@
+def dfs_n_queens(n):
+
+    if n < 1 : 
+        return []
+    if n < 4:
+        return []
+    if n ==1:
+        return [[0]]
+    matrix = [[0 for _ in range(n)] for _ in range(n)]
+    
+    
+
+    return matrix
+
+
+
 # Queens of the same color don't hit each other, that's the solution, guys... (joke)
